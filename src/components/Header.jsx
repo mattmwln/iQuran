3b13f653a2665589b60fc6456dbc221bd3ed6ff8
Rahmat Maulana <132111224+mattmwln@users.noreply.github.com>
2024-06-22 17:39:38 +0700

    Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,9 +9,9 @@ function Header() {
       <div className="header-overlay"></div>
       <nav className="container mx-auto flex items-center py-4 relative z-10 header-nav">
         <div className="flex">
-          <Link to='/' className="header-link">Home</Link>
-          <Link to='/features' className="header-link">Partner</Link>
-          <Link to='/contact' className="header-link">Contact</Link>
+          <Link to='/' className="header-link" title="Go to Home">Home</Link>
+            <Link to='/features' className="header-link" title="Explore Features">Partner</Link>
+            <Link to='/contact' className="header-link" title="Contact Us">Contact</Link>
         </div>
       </nav>
     </header>
